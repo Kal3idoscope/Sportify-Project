69de0928de677d7fd6af390d2f02cc0c71afb9c4
NagiAiza <67315055+NagiAiza@users.noreply.github.com>
2023-05-30 11:13:53 +0200

    login

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sportify</title>
+    <link rel="stylesheet" href="styles/pagecoach.css" type="text/css" />
+    <link rel="stylesheet" href="styles/home.css" type="text/css" />
+    <link rel="stylesheet" href="styles/general.css" type="text/css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
