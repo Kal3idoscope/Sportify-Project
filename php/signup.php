5aeadec2b6a38435ec9390eaa5f0ab00c3c8a24f
NagiAiza <67315055+NagiAiza@users.noreply.github.com>
2023-05-30 12:31:32 +0200

    signup done

--- php/signup.php
+++ php/signup.php
@@ -1,14 +1,17 @@
 <?php
 session_start();
 include_once "config.php";
-$fname = mysqli_real_escape_string($conn, $_POST['fname']);
-$lname = mysqli_real_escape_string($conn, $_POST['lname']);
+$prenom = mysqli_real_escape_string($conn, $_POST['prenom']);
+$nom = mysqli_real_escape_string($conn, $_POST['nom']);
 $email = mysqli_real_escape_string($conn, $_POST['email']);
 $password = mysqli_real_escape_string($conn, $_POST['password']);
-if (!empty($fname) && !empty($lname) && !empty($email) && !empty($password)) {
+if (!empty($prenom) && !empty($nom) && !empty($email) && !empty($password)) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
-        if (mysqli_num_rows($sql) > 0) {
+        //$sql = mysqli_query($conn, "SELECT * FROM admin WHERE email = '{$email}' UNION SELECT * FROM client WHERE email = '{$email}' UNION SELECT * FROM coach WHERE email = '{$email}'");
+        $sql_admin = mysqli_query($conn, "SELECT * FROM admin WHERE email = '{$email}'");
+        $sql_client = mysqli_query($conn, "SELECT * FROM client WHERE email = '{$email}'");
+        $sql_coach = mysqli_query($conn, "SELECT * FROM coach WHERE email = '{$email}'");
+        if (mysqli_num_rows($sql_admin) > 0 || mysqli_num_rows($sql_client) > 0 || mysqli_num_rows($sql_client) > 0 ) {
             echo "$email - This email already exist!";
         } else {
             if (isset($_FILES['image'])) {
@@ -25,16 +28,37 @@ if (!empty($fname) && !empty($lname) && !empty($email) && !empty($password)) {
                     if (in_array($img_type, $types) === true) {
                         $time = time();
                         $new_img_name = $time . $img_name;
-                        if (move_uploaded_file($tmp_name, "images/" . $new_img_name)) {
+                        if (move_uploaded_file($tmp_name, "pic/" . $new_img_name)) {
                             $ran_id = rand(time(), 100000000);
                             $status = "Active now";
                             $encrypt_pass = md5($password);
-                            $insert_query = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status)
-                                VALUES ({$ran_id}, '{$fname}','{$lname}', '{$email}', '{$encrypt_pass}', '{$new_img_name}', '{$status}')");
+                           /*$insert_query = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status)
+                                VALUES ({$ran_id}, '{$prenom}','{$nom}', '{$email}', '{$encrypt_pass}', '{$new_img_name}', '{$status}')");*/
+                            if (mysqli_num_rows($sql_admin) > 0) {
+                                $insert_query = mysqli_query($conn, "INSERT INTO admin (ID_admin , Nom, Prenom, Email, Pass, Photo)
+                            VALUES ('{$ran_id}', '{$nom}', '{$prenom}', '{$email}', '{$encrypt_pass}', '{$new_img_name}')");
+                            } elseif (mysqli_num_rows($sql_client) > 0) {
+                                $insert_query = mysqli_query($conn, "INSERT INTO client (ID_Coach , Nom, Prenom, Email, Pass, Photo)
+                            VALUES ('{$ran_id}', '{$nom}', '{$prenom}', '{$email}', '{$encrypt_pass}', '{$new_img_name}')");
+                            } elseif (mysqli_num_rows($sql_coach) > 0) {
+                                $insert_query = mysqli_query($conn, "INSERT INTO coach (ID_Client , Nom, Prenom, Email, Pass, Photo)
+                            VALUES ('{$ran_id}', '{$nom}', '{$prenom}', '{$email}', '{$encrypt_pass}', '{$new_img_name}')");
+                            }
+
                             if ($insert_query) {
-                                $select_sql2 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
-                                if (mysqli_num_rows($select_sql2) > 0) {
-                                    $result = mysqli_fetch_assoc($select_sql2);
+                                $select_sql_admin = mysqli_query($conn, "SELECT * FROM admin WHERE email = '{$email}'");
+                                $select_sql_client = mysqli_query($conn, "SELECT * FROM client WHERE email = '{$email}'");
+                                $select_sql_coach = mysqli_query($conn, "SELECT * FROM coach WHERE email = '{$email}'");
+                                if (mysqli_num_rows($select_sql_admin) > 0) {
+                                    $result = mysqli_fetch_assoc($select_sql_admin);
+                                    $_SESSION['unique_id'] = $result['unique_id'];
+                                    echo "success";
+                                } else if (mysqli_num_rows($select_sql_client) > 0){
+                                    $result = mysqli_fetch_assoc($select_sql_client);
+                                    $_SESSION['unique_id'] = $result['unique_id'];
+                                    echo "success";
+                                } else if (mysqli_num_rows($select_sql_coach) > 0){
+                                    $result = mysqli_fetch_assoc($select_sql_coach);
                                     $_SESSION['unique_id'] = $result['unique_id'];
                                     echo "success";
                                 } else {
