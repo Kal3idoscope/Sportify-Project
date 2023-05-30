b50f934685e30baba4270306960ee821211c8c01
biblekid3 <94248399+biblekid3@users.noreply.github.com>
2023-05-30 21:25:13 +0200

    PUSH

--- login.php
+++ login.php
@@ -21,28 +21,52 @@ if (isset($_SESSION['unique_id'])) {
 
     <?php include_once "header.php"; ?>
     <section class="form login">
+    <head>
+      <meta charset="UTF-8">
+      <title>Home</title>
+      <link rel="stylesheet" href="styles/connexion.css" type="text/css" />
+      <link rel="stylesheet" href="styles/general.css" type="text/css" />
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;500&family=Shrikhand&display=swap" rel="stylesheet">
+
+    </head>
     <body>
-    <div class="wrapper">
-        <section class="form login">
-            <header>Wsh ma parole</header>
+    <div class="blocHeader">
+      <div class="bloc1">
+        <section class="formLogin">
+            <h1 class="titre">Wsh ma parole</h1>
             <form action="php/login.php" method="POST" enctype="multipart/form-data" autocomplete="off">
                 <div class="error-text"></div>
-                <div class="field input">
-                    <label>Adresse Email</label>
-                    <input type="text" name="email" placeholder="Saisir votre email" required>
+                <div class="fieldInput">
+                    <label>Adresse Email : </label>
+                    <input class="in"  type="text" name="email"required>
                 </div>
-                <div class="field input">
-                    <label>Mot de Passe</label>
-                    <input type="password" name="password" placeholder="Saisir votre password" required>
+                <div class="fieldInput">
+                    <label>Mot de Passe : </label>
+                    <input class="in" type="password" name="password"  required>
                     <i class="fas fa-eye"></i>
                 </div>
-                <div class="field button">
-                    <input type="submit" name="submit" value="Se Connecter">
+                <div class="fieldButton">
+                    <input class="bouton" type="submit" name="submit" value="Se Connecter">
                 </div>
             </form>
             <div class="link">Déjà inscrit? <a href="signup.php">S'inscrire</a></div>
         </section>
     </div>
+    <div class="bloc2">
+
+        <div class="barrerecherche">
+          <input class="rechercher" type="text" placeholder="Search..">
+        </div>
+        <div class="header">
+          <a href="./index.html">ACCUEIL</a> <br>
+          <a href="./Parcourir.html">TOUT PARCOURIR</a> <br>
+          <a href="./RDV.html">RDV</a> <br>
+          <a href="./COMPTE.html">COMPTE</a> <br>
+        </div>
+      </div>
+      </div>
 
     <script src="javascript/pass-show-hide.js"></script>
     <script src="javascript/login.js"></script>
