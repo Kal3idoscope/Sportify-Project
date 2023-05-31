6f97e94f54ed034f9c27d5b5af46e264c5e1d9f3
Yolaine <yolaine.drouart@edu.ece.fr>
2023-05-31 16:06:42 +0200

    féf

--- admin.php
+++ admin.php
@@ -7,9 +7,27 @@ if(!isset($_SESSION['unique_id'])){
 ?>
 <?php include_once "header.php"; ?>
 <link rel="stylesheet" href="styles/admin.css" type="text/css" />
-
-
-
+<link rel="stylesheet" href="styles/scrollmenu.css" type="text/css" />
+<script>
+    /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+</script>
 <body>
 <?php
 $sql = mysqli_query($conn, "SELECT * FROM admin WHERE ID_Admin = {$_SESSION['unique_id']}");
@@ -58,8 +76,8 @@ if(mysqli_num_rows($sql) > 0){
             <a href="./rdv.php">RDV</a><br>
             <a href="./login.php">COMPTE</a><br>
         </div>
-        <div class="chat">
-             <button><a href="php/logout.php?logout_id=<?php echo $row['ID_Admin']; ?>" class="logout">LOG <br> OUT</a></button>
+        <div class="boutonLOGOUT">
+             <button style="width:100% ; height:95%; font-size: 200%"><a href="php/logout.php?logout_id=<?php echo $row['ID_Admin']; ?>" class="logout">LOG <br> OUT</a></button>
         </div>
     </div>
 </div>
--- client.php
+++ client.php
@@ -1,14 +1,33 @@
 <?php
 session_start();
 include_once "php/config.php";
-if (!isset($_SESSION['unique_id'])) {
+if(!isset($_SESSION['unique_id'])){
     header("location: login.php");
 }
 ?>
 <?php include_once "header.php"; ?>
-<link rel="stylesheet" href="styles/client.css" type="text/css"/>
-
-
+<link rel="stylesheet" href="styles/client.css" type="text/css" />
+<link rel="stylesheet" href="styles/scrollmenu.css" type="text/css" />
+<script>
+    /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+</script>
 <body>
 <?php
 $sql = mysqli_query($conn, "SELECT * FROM client WHERE ID_Client = {$_SESSION['unique_id']}");
@@ -18,9 +37,8 @@ if (mysqli_num_rows($sql) > 0) {
 ?>
 <div class="blocHeader">
     <div class="bloc1">
-        <h1 class="titre">COMPTE CLIENT
-            <h1>
-                <br>
+        <h1 class="titre">COMPTE CLIENT <h1>
+        <br>
                 <div class="blocProfil">
                     <img class="photodeprofil" src="Images/mouhali.jpeg" alt="mouhali"/>
                     <div class="profildata">
@@ -34,13 +52,13 @@ if (mysqli_num_rows($sql) > 0) {
 
                     </div>
                 </div>
-                <div class="option1">
-
-                    <button><a href="./change_info_admin.php">MODIFIER MES INFOS</a></button>
-                    <button>PRENDRE UN RDV</button>
-                    <button>HISTORIQUE DE MES RDV</button>
+                <div class="option7">
+                            <button><a href="./change_info_admin.php">MODIFIER MES INFOS</a></button>
+                            <button>PRENDRE UN RDV</button>
+                            <button>HISTORIQUE DE MES RDV</button>
                 </div>
-    </div>
+            </div>
+
 
 
     <div class="bloc2">
@@ -61,10 +79,9 @@ if (mysqli_num_rows($sql) > 0) {
             <a href="./RDV.html">RDV</a><br>
             <a href="./login.php">COMPTE</a><br>
         </div>
-        <div class="chat">
-            <button><a href="php/logout.php?logout_id=<?php echo $row['ID_Client']; ?>" class="logout">LOG <br> OUT</a>
-            </button>
-        </div>
+        <div class="boutonLOGOUT">
+                     <button style="width:100% ; height:95%; font-size: 200%" ><a href="php/logout.php?logout_id=<?php echo $row['ID_Client']; ?>" class="logout">LOG OUT</a></button>
+                </div>
     </div>
 </div>
 <div class="RDV">
