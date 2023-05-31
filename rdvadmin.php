0cca4d6b7dc7b2d83c169fa6f5d33f78bbbc1ba7
biblekid3 <94248399+biblekid3@users.noreply.github.com>
2023-05-31 15:20:48 +0200

    PUSH

---
+++ rdvadmin.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+include_once "php/config.php";
+if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+}
+?>
+<?php include_once "header.php"; ?>
+<link rel="stylesheet" href="styles/admin.css" type="text/css" />
+
+
+
+<body>
+<?php
+$sql = mysqli_query($conn, "SELECT * FROM admin WHERE ID_Admin = {$_SESSION['unique_id']}");
+if(mysqli_num_rows($sql) > 0){
+    $row = mysqli_fetch_assoc($sql);
+}
+?>
+
+<div class="blocHeader">
+    <div class="bloc1">
+        <h1 class="titre" style="padding:10%; line-height:190%">VOUS ETES ADMINISTRATEUR VOUS N'AVEZ PAS DE RENDEZ-VOUS <h1>
+            </div>
+
+
+
+    <div class="bloc2">
+
+        <div class="barrerecherche">
+            <input class="rechercher" type="text" placeholder="Search..">
+        </div>
+        <div class="header">
+            <a href="./index.html">ACCUEIL</a>
+            <div class="dropdown">
+                <div onclick="myFunction()" class="dropbtn">TOUT PARCOURIR</div>
+                <div id="myDropdown" class="dropdown-content">
+                    <a href="./Activites_Sportives.html">ACTIVITÉS SPORTIVES</a>
+                    <a href="./Sport_Compet.html">SPORTS DE COMPÉTITION</a>
+                    <a href="./salleOmnes.html">SALLES DE SPORT OMNES</a>
+                </div>
+            </div>
+            <a href="./rdv.php">RDV</a><br>
+            <a href="./login.php">COMPTE</a><br>
+        </div>
+        <div class="chat">
+             <button><a href="php/logout.php?logout_id=<?php echo $row['ID_Admin']; ?>" class="logout">LOG <br> OUT</a></button>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
